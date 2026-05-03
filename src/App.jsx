eb929da69b1cfc8e936040d0ddcac8a3a88868a8
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-05-03 18:18:13 +0600

    React project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <>
+    <div className="text-red-500">helo</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#EFEEEC",
+        heading: "#111212",
+        nav: "#BEBEBE",
+        footer: "#282828",
+        mint: "#B2F6E3",
+
+        hoverPink1: "#FECACC",
+        hoverPink2: "#FDD8C4",
+        hoverPurple: "#D29DD0",
+        hoverAsh: "#D8C4FD",
+        hoverBlueDark: "#3A8CCB",
+        hoverBlueLight: "#60DCFB",
+        hoverBlue: "#39B0BD",
+        hoverOrange: "#CB7B3A",
+        hoverGrayPink: "#D2B59D",
+      },
+      fontFamily: {
+        sans: ["saans", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
